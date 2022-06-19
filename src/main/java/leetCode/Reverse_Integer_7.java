258a153a8b98cf431179bb46a7443db957a2478e
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2022-06-19 21:14:52 +0600

    solve Reverse_Integer_7 using long data type

--- src/main/java/leetCode/Reverse_Integer_7.java
+++ src/main/java/leetCode/Reverse_Integer_7.java
@@ -10,24 +10,19 @@ public class Reverse_Integer_7 {
        double minValue = -(Math.pow(2,31));
     }
 
-    public static int reverse(int x){
-        int reversed = 0;
+    public static int reverse(int x) {
+        long reversed = 0;
         int pop;
 
         while (x !=0){
             pop = x % 10;
             x = x/10;
-
-
-
-            if(reversed>Integer.MAX_VALUE/10 || reversed == Integer.MAX_VALUE/10 && pop > 7)
-                return 0;
-            if(reversed<Integer.MIN_VALUE/10 || reversed == Integer.MIN_VALUE/10 && pop <-8)
-                return 0;
-
             reversed  = (reversed * 10) + pop;
+            if (reversed > Integer.MAX_VALUE || reversed<Integer.MIN_VALUE ) {
+                return 0;
+            }
         }
 
-        return reversed;
+        return (int) reversed;
     }
 }
--- src/main/java/leetCode/Rotate_Image.java
+++ src/main/java/leetCode/Rotate_Image.java
@@ -1,5 +1,38 @@
 package leetCode;
 
 public class Rotate_Image {
-    
+
+    public static void main(String[] args) {
+       int matrix[][] = {
+                            {5,1,9,11},
+                            {2,4,8,10},
+                            {13,3,6,7},
+                            {15,14,12,16}
+                        };
+
+       Rotate_Image.rotate(matrix);
+    }
+
+    public static void rotate(int[][] matrix){
+        int length = matrix.length;
+
+        for(int row=0; row<length; row++){
+            for(int col=row; col<length; col++){
+                int temp = matrix[row][col];
+                System.out.println("Temp value: " + temp);
+
+                matrix[row][col] = matrix[col][row];
+                System.out.println("Diagonal value: " +  matrix[col][row]);
+                matrix[col][row] = temp;
+            }
+        }
+
+        for(int i=0; i<length; i++){
+            for(int j=0; j<(length/2); j++ ){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][length-1-j];
+                matrix[i][length-1-j] = temp;
+            }
+        }
+    }
 }
