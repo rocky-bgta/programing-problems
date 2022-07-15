72c4f669506d6eb67a79601e6b4ac206e0a9d6fd
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2022-07-15 09:41:39 +0600

    marge two shorted array

--- src/main/java/leetCode/BinaryTreeDemo.java
+++ src/main/java/leetCode/BinaryTreeDemo.java
@@ -242,6 +242,20 @@ public class BinaryTreeDemo {
         }
         return true;
     }
+
+    public TreeNode sortedArrayToBST(int[] nums){
+        if(nums.length == 0) return null;
+        return constructTreeFromArray(nums,0,nums.length-1);
+    }
+
+    public TreeNode constructTreeFromArray(int[] nums, int left, int right){
+        if(left>right) return null;
+        int midpoint = left + (right-left)/2;
+        TreeNode node = new TreeNode(nums[midpoint]);
+        node.left = constructTreeFromArray(nums,left,midpoint-1);
+        node.right = constructTreeFromArray(nums,midpoint+1,right);
+        return node;
+    }
     
     public void postOrder(TreeNode root) {
 		if(root == null) {
