e9e5e937ee64c7930a592c1853f90a562c49a357
Salahin <rocky.bgta@gmail.com>
2022-07-19 12:42:40 +0600

    ContainsDuplicate.java explanation

--- src/main/java/leetCode/ContainsDuplicate.java
+++ src/main/java/leetCode/ContainsDuplicate.java
@@ -5,6 +5,24 @@ import java.util.Map;
 
 public class ContainsDuplicate {
 
+    /*
+    Here trick is we put array value for both key and value of HashMap
+
+    Example 1:
+
+    Input: nums = [1,2,3,1]
+    Output: true
+    Example 2:
+
+    Input: nums = [1,2,3,4]
+    Output: false
+    Example 3:
+
+    Input: nums = [1,1,1,3,3,4,3,2,4,2]
+    Output: true
+
+     */
+
     public boolean containsDuplicate(int[] nums) {
 
         Map<Integer,Integer> holder = new HashMap<>();
