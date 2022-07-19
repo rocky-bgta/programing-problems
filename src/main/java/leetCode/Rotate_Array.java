4a97ca886a45efe2f9b90b694b19621830b287e5
Salahin <rocky.bgta@gmail.com>
2022-07-19 12:17:16 +0600

    Rotate_Array.java explanation

--- src/main/java/leetCode/Rotate_Array.java
+++ src/main/java/leetCode/Rotate_Array.java
@@ -1,7 +1,15 @@
 package leetCode;
 
 public class Rotate_Array {
-
+//*) Let us consider this with an example : [1,2,3,4], k=2; output = [3,4,1,2]
+//            *)  The intution is simple,  just the rotate the array.
+//            *) Rotation one from 0 to n(where n is length of array)
+//    After rotation one : [4,3,2,1]
+//            *) Rotation two from 0 to k-1,
+//    After rotation two : [3,4,2,1]
+//            *) Rotation three from k to n,
+//    After rotation three : [3,4,1,2]
+//            *) And here comes the output array, [3,4,1,2]
     public static void main(String[] args) {
         int[] nums = {1,2,3,4,5,6,7,8};
         int k = 10;
