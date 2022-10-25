a9918738728ff736e60c2a5032743dac782efb3d
Md. Nazmus Salahin Rocky <salahin.rocky@tslc.co.in>
2022-10-25 22:13:15 +0600

    update MergeSortedArrays.java and FizzBuzz.java

--- src/main/java/leetCode/MergeSortedArrays.java
+++ src/main/java/leetCode/MergeSortedArrays.java
@@ -6,38 +6,17 @@ public class MergeSortedArrays {
 //    Output: [1,2,2,3,5,6]
 //    Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
 //    The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+    //https://www.youtube.com/watch?v=C4oBXLr3zos
 
-    public int[] merge(int[] nums1, int m, int[] nums2, int n){
-
-        int i = m-1;
-        int j = n-1;
-        int k = (m+n -1);
-
-        System.out.println("Value of K: " + k);
-
-        while (i>=0 && j>=0){
-            if(nums1[i] > nums2[j]){
-                nums1[k] = nums1[i];
-                i--;
+    public void merge(int[] nums1, int m, int[] nums2, int n){
+        int p1= m-1, p2 = n-1, i = m+n -1;
+        while (p2>=0){
+            if(p1>=0 && nums1[p1]>nums2[p2]){
+                nums1[i--] = nums1[p1--];
             }else {
-                nums1[k] = nums2[j];
-                j--;
+                nums1[i--] = nums2[p2--];
             }
-            k--;
         }
-
-        while (i>=0){
-            nums1[k] = nums1[i];
-            i--;
-            k--;
-        }
-        while (j>=0){
-            nums1[k--] = nums2[j];
-            j--;
-            k--;
-        }
-
-        return nums1;
     }
 
     public void printArray(int[] arr) {
@@ -55,8 +34,8 @@ public class MergeSortedArrays {
         msa.printArray(nums1);
         msa.printArray(nums2);
 
-        int[] result = msa.merge(nums1, 3, nums2, 3);
-        msa.printArray(result);
+       // int[] result = msa.merge(nums1, 3, nums2, 3);
+        //msa.printArray(result);
     }
 
 }
