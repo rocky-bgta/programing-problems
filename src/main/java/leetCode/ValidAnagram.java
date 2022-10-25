839b82d17b1e5421f3c3052dd38179a193d7ff56
Md. Nazmus Salahin Rocky <salahin.rocky@tslc.co.in>
2022-10-25 19:44:17 +0600

    update ValidAnagram

--- src/main/java/leetCode/ValidAnagram.java
+++ src/main/java/leetCode/ValidAnagram.java
@@ -1,5 +1,7 @@
 package leetCode;
 
+import java.util.Arrays;
+
 public class ValidAnagram {
 
     public static void main(String[] args) {
@@ -9,24 +11,31 @@ public class ValidAnagram {
        ValidAnagram.isAnagram(s,t);
     }
 
-    public static boolean isAnagram(String str1, String str2){
-        if(str1.length() != str2.length()){
+    public static boolean isAnagram(String s, String t){
+
+        char[] str1;
+        char[] str2;
+
+        str1 = s.toCharArray();
+        str2 = t.toCharArray();
+        // Get lengths of both strings
+        int n1 = str1.length;
+        int n2 = str2.length;
+
+        // If length of both strings is not same,
+        // then they cannot be anagram
+        if (n1 != n2)
             return false;
-        }
 
-        int [] countArr = new int[26];
+        // Sort both strings
+        Arrays.sort(str1);
+        Arrays.sort(str2);
 
-        for (int i=0; i<str1.length(); i++){
-            countArr[str1.charAt(i) - 'a']++;
-
-            countArr[str2.charAt(i) - 'a']--;
-        }
-
-        for(int j=0; j<countArr.length; j++){
-            if (countArr[j] != 0){
+        // Compare sorted strings
+        for (int i = 0; i < n1; i++)
+            if (str1[i] != str2[i])
                 return false;
-            }
-        }
+
         return true;
     }
 }
