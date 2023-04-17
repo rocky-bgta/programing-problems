ff3e0f8dbcf5c260c7a39e7273ab12d1ce0ff0d6
Salahin <rocky.bgta@gmail.com>
2023-04-17 20:15:33 +0600

    two way done

--- src/main/java/rnd_en_dn/newone/CryptoHelper.java
+++ src/main/java/rnd_en_dn/newone/CryptoHelper.java
@@ -1,11 +1,14 @@
 package rnd_en_dn.newone;
 
 import javax.crypto.Cipher;
+import javax.crypto.spec.OAEPParameterSpec;
+import javax.crypto.spec.PSource;
 import java.io.File;
 import java.nio.file.Files;
 import java.security.KeyFactory;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.spec.MGF1ParameterSpec;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
@@ -16,6 +19,8 @@ import java.util.Base64;
 public class CryptoHelper {
     // https://mkyong.com/java/java-asymmetric-cryptography-example/
     // https://stackoverflow.com/questions/44681737/get-a-privatekey-from-a-rsa-pem-file
+
+    private static String node_rsa_init = "RSA/ECB/OAEPWithSHA-256AndMGF1Padding";
   
     public static void main(String[] args){
   
@@ -25,43 +30,57 @@ public class CryptoHelper {
             //String privateKeyfile = "../pvkey.pem";
             // https://acte.ltd/utils/openssl
 
+          /*  # Private key
+            openssl genpkey -algorithm RSA -out private.pem -pkeyopt rsa_keygen_bits:2048
 
-            String pubkeyfile = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1IkMkTduzACmgj6b8cvR" +
-                    "VXQ0Mun1/e1jxEmWy4Th7s/Hul5JXr9r4JCS6COWWsLnAd2QyPYu06YuSz6jgg2c" +
-                    "yM+aor8BnMMQJfGVx1A8uqkAOin0BmHu7jagmS4Lp+z+NozyQRaD5rI9BcQbIrZt" +
-                    "jkKADen+OWNYhuYatevoqPHeuLklNr7vcf87pZMCnc5LIsUh4gb8jP+S2kirEV8x" +
-                    "HVsQpLQ8D4vbGJIThOP/elIzI5W3lMMSwcrOJa7KQjAGE2yqCy9dIHdK4y0R41mF" +
-                    "hjoP3dJ1yW1LkVYtsuUpP825glQguG46xC6bQvRkuJ3h1Kr/wyUpvvpEGdE9qVqt" +
-                    "hwIDAQAB";
+# Public key
+            openssl rsa -pubout -in private.pem -out public_key.pem
+
+# Private key in pkcs8 format (for Java maybe :D)
+            openssl pkcs8 -topk8 -in private.pem -out private_key.pem
+
+## nocrypt (Private key does have no password)
+            openssl pkcs8 -topk8 -in private.pem -nocrypt -out private_key.pem
+*/
+
+            String pubkeyfile =
+                    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArUqb0FiaIv/sEnTo1J8z" +
+                    "rAA2nBJt3oRqI83kJcrZ7hwVYy0mI+QG+wxFmMUah8dZ1sIB6G5R+6zG4vJpXyYF" +
+                    "pw2lWcWAKZzu6uKub8KxFHMbrCLyoH5sxZlEz+YThZK6u04EHHLnq7q5UKbmyL/H" +
+                    "y08IlJzuTUfEf8XUrIXSRub6hCUYIQnbxK2EiYwApFwd0dAsxzegEcdnA7sg2066" +
+                    "50VphL2btRqJ57g2zcta6bQNjo+G6MUrvXbGtvb3s1F6tynxufj+sqig3UrDTgBV" +
+                    "AkunPKHWSndR8fTzajF29QNXXJMQYrDmbSu3GR5Tk64uBrIuHo8vHFeCxFr5rlJQ" +
+                    "DwIDAQAB";
 
 
 
-            String privateKeyfile = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDUiQyRN27MAKaC" +
-                    "Ppvxy9FVdDQy6fX97WPESZbLhOHuz8e6Xklev2vgkJLoI5ZawucB3ZDI9i7Tpi5L" +
-                    "PqOCDZzIz5qivwGcwxAl8ZXHUDy6qQA6KfQGYe7uNqCZLgun7P42jPJBFoPmsj0F" +
-                    "xBsitm2OQoAN6f45Y1iG5hq16+io8d64uSU2vu9x/zulkwKdzksixSHiBvyM/5La" +
-                    "SKsRXzEdWxCktDwPi9sYkhOE4/96UjMjlbeUwxLBys4lrspCMAYTbKoLL10gd0rj" +
-                    "LRHjWYWGOg/d0nXJbUuRVi2y5Sk/zbmCVCC4bjrELptC9GS4neHUqv/DJSm++kQZ" +
-                    "0T2pWq2HAgMBAAECggEBAKxiTJMsRu2oSWWyqoHAMxYAnqX9JczD0vvORETmjupI" +
-                    "FVOQTipPUIxgNweadcU/+GaG83xMUUbRrxLQx4YgzF4EJQSDsCcZ/+sGu9lhtyAf" +
-                    "G1QR2x+iJ02HeVNFCac2jipov8UTIgDn6J7YpLnPBaUxQvQGKnEZpyAjWt7bbWY4" +
-                    "7WvuyiubBkCzPPwZ6ySEpeqy35rMRoc4a6RLmeMiN7RwQLU3qBefdv3fqXYASJtK" +
-                    "oxiwkkt0aB2oEZhFOJ5CuS0+go2HrBanXZIr8rMDIdkxqs2FM9kdDxJ+PEzkOr+X" +
-                    "gXe1jOC0fxtzKVZHPP1dRtmnIGL/acWheHK9s9VbqeECgYEA6qgGT/GscTXUG+NU" +
-                    "COTqGTc//e3QlXJzTdLREt0x8Rf30qy8XMP/083RJb4996uq/+b8UU9DUD7gIT5a" +
-                    "SOaIH1eOjkNT6186LL3JXwbCzbUUgJ+WnsY8o0diBdqU63lAcLxq64KC2x5cAujQ" +
-                    "oVxfhvnlBUHbTMjJ0QWt479z8H0CgYEA593v0B1h7dovLj+fKh7964hEk1SmILwL" +
-                    "Je78d6xEszUQ/5C2D1fW7w0nVvgNbWhsJPIvDCxijr0rmU96LnehZIwr2/Vs3GOx" +
-                    "9AIXI56C2kdfXvim5yUDbKs2LerXyPCiC7qlbyBz4S3zZeSCuBtymRUnNrHpdkxr" +
-                    "NtHXEot9mVMCgYEAki2d0QtiOxwnSEAzR18jHhY2/P66435qqd5cc/3rcJ9XuJv2" +
-                    "s2s94Tnjwjli7Pi6C7RfFl/kNQcWJF9tauCBh9TJb4ItswCdguWvnkzd6buWTBIe" +
-                    "iEZB3A1J2Q+zmYTtSc12ZcPltI6TDxTtcrfSoojlzIKcFJc4ccex7aCEnxkCgYAR" +
-                    "HPNHM/P3XyQeULnJnRH2oU8WZKgCQH6+JP5jQsarR15izmZxiXUmnZzqsVIkYhGh" +
-                    "RHKuflQj3thp71DAPzodNq9CA8IuHfmTXdghW+CihaQHWWjQJI41JgnQp8WXYhND" +
-                    "tFnjcsy6sO6G9a+kMPprTAFh6w2lGozLNf+escbqWwKBgBT/ZrBpNpIKW8Mtib7A" +
-                    "MZPgJZIqEbF6NS3m/fNMLi7Jd5Qb8CPPxtwkw4ttWo8b8y2cmvCy8VocSbZNkpZt" +
-                    "ikxyg/8lUnt+A6rWrKQAWHsSYtFuzIlZE6ywv/osBM249AdRvI0xaDah21T9jK2s" +
-                    "93clqBTtnZUo3W8k246xTGby";
+            String privateKeyfile =
+                    "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCtSpvQWJoi/+wS" +
+                    "dOjUnzOsADacEm3ehGojzeQlytnuHBVjLSYj5Ab7DEWYxRqHx1nWwgHoblH7rMbi" +
+                    "8mlfJgWnDaVZxYApnO7q4q5vwrEUcxusIvKgfmzFmUTP5hOFkrq7TgQccuerurlQ" +
+                    "pubIv8fLTwiUnO5NR8R/xdSshdJG5vqEJRghCdvErYSJjACkXB3R0CzHN6ARx2cD" +
+                    "uyDbTrrnRWmEvZu1GonnuDbNy1rptA2Oj4boxSu9dsa29vezUXq3KfG5+P6yqKDd" +
+                    "SsNOAFUCS6c8odZKd1Hx9PNqMXb1A1dckxBisOZtK7cZHlOTri4Gsi4ejy8cV4LE" +
+                    "WvmuUlAPAgMBAAECggEAKZMLvLl1CU6S6X6o2VH0iKBoeZsmRstH150uzi5TiH+u" +
+                    "VePgEZ5EWniAPf2Ttt2J3S8gXA4TlyjFcm8r/orVslVgxR/P9qvS2gFWTjPMXhab" +
+                    "nfmocf7Cw8lYq/QL3PDt4b/+1oBuulGK4iGYnrL8EPe+HEFmHp5IM0tYEaftYdrl" +
+                    "MHeFix7JTUZmiIubS50hdfnMdsNFkKoUjRJADlrFmcLP3+XFAY0fa8VZ3gQryIUN" +
+                    "mKB10pQDdUPqrFeIgkADG2aHLi9eahtX4nNUutNAskk52LRbeCEUVtu2CfpALXA5" +
+                    "3w6cp4D+RVNAb6EuZZu7AO5V1qvoByODSd0araHlwQKBgQDdd0GuazTdb6s5pYH0" +
+                    "6OJkv8DWyf9rtyqy6UZawE0RK47c+85mZNtTsQlmyXOuBT9Z/R+fbjNgcHY/J7ad" +
+                    "JRvXeCs3Cr+zuJ3EV2jaGnmZ3dRxXX4TA2ctjWJc7Md1bgotwTgb/bmATD4bkbOI" +
+                    "DRyUbM0jGxTkCcCYOQlBQ2CkYQKBgQDIUER4E6SQdgA6VT6wD88FwaffqNnk/0Kg" +
+                    "7+YHTfi5mXMEhVNcE00/SMrCFwiQodEZZnO5H6d2+O7ALwnUAW2LOTy4nxAwCevU" +
+                    "CyE+mOGbpP7/yMwyLozFtvlxMXRTfssQQFkWQkJZHT1s2cseQd+Ytg6MngNgE1Rl" +
+                    "0xErkUdKbwKBgHbOwWemj6ezfnHgfkU6oilWTUds+h95FekBCAMrYqyxZ2TZmTKJ" +
+                    "LLhLIzYHII3cts254zsOTXWRH35l+at3+uWuy9AtbgYmJkzsb42B4nZO8OcYNshQ" +
+                    "bQuc64XaaG5w5/j/zPAHP3q6bB8yHNQB5LEjZTFOF/j12mJPRs3CLZhBAoGBAK/F" +
+                    "348OjYVGRoTpcIzGEn+Wo1ybqjhL1Yere0pFnVAf2tHIvJE7TrmNgn42AAoKNJX5" +
+                    "VOrJYAYCAl8Z56Hlbt6QZzwKOAYvboUvDwkC1lXhvvFsneIa/uQv30r4fn8V79Je" +
+                    "5M2nbtkL027E3nu6ih5TAwlZTtTroU+ImrSEUu55AoGAb9/WeQV2C5JiA93WIcFg" +
+                    "JtFDO1cYNFpYvVqpAuRnfBes2VBxCxVfP5CdroNzYdj5qroyMsZDoCGsiMwkIp2i" +
+                    "CRwSBh8kNj7sut8Gd9Xw3oe5o9VTrZRbQeKfDZBlVwLXjjwF2AaHZi7ChllmwNUV" +
+                    "Q+xK7RaJrgbd9uIqQR6Ehoo=";
 
 
 
@@ -73,10 +92,12 @@ public class CryptoHelper {
             // encrypt
         String s = "The quick brown fox jumps over the lazy dog"; 
             String enc = CryptoHelper.encryptStringWithPublicKey(s, pubkeyfile);
-            System.out.println( String.format("%s -> %s", s, enc));
+            //System.out.println( String.format("%s -> %s", s, enc));
   
             // decrypt
-            String dec = CryptoHelper.decryptStringWithPrivateKey(enc, privateKeyfile);
+            String nodeJSEncrypted="O9UhqjomngHWkLzSsyMurY96KLgDWTxxqc37KLrQoUryTy1c6smpRnVKAfikEPNCpUpAjTaHtUseR+9Mk/O5HvlIfovn1vuwyBq7u6DMf2NWKjZswvU14N+Cp3E2OMVb5biWJZRQs9htG7/A7oW2pxW3DGZxF0yftxJZNryICQmydj73+kq0anG15pyNwnyID2uu4/tngyABxOfSf+f5Yf/yDeKXEMDlcxMfnCxjnKhn1C+uufxG0PGGgYev9lYNQBPkZMQ0Lfhm/VGDulfUTJ6VNYnCxqM6t1t4ixJyjrBglTJdStMwP+FuVBf9w3LKd9G3gzNvTgyg+RV2S9h8+A==";
+
+            String dec = CryptoHelper.decryptStringWithPrivateKey(nodeJSEncrypted, privateKeyfile);
             System.out.println( String.format("%s -> %s", enc, dec));
         }
         catch(Exception ex){
@@ -86,17 +107,22 @@ public class CryptoHelper {
     }
  
     public static String encryptStringWithPublicKey(String s, String keyFilename) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA");
+        Cipher cipher = Cipher.getInstance(node_rsa_init);
         PublicKey pubkey = readPublicKeyFromPem(keyFilename);
-        cipher.init(Cipher.ENCRYPT_MODE, pubkey);
+        // encrypt
+        // cipher init compatible with node.js crypto module!
+        cipher.init(Cipher.ENCRYPT_MODE, pubkey,
+                new OAEPParameterSpec("SHA-256", "MGF1", MGF1ParameterSpec.SHA256, PSource.PSpecified.DEFAULT));
         String enc = Base64.getEncoder().encodeToString(cipher.doFinal(s.getBytes("UTF-8")));
         return enc;
     }
   
     public static String decryptStringWithPrivateKey(String s, String keyFilename)  throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA");
+        Cipher cipher = Cipher.getInstance(node_rsa_init);
         PrivateKey pkey = readPrivateKeyFromPem(keyFilename);
-        cipher.init(Cipher.DECRYPT_MODE, pkey);
+        // cipher init compatible with node.js crypto module!
+        cipher.init(Cipher.DECRYPT_MODE, pkey,
+                new OAEPParameterSpec("SHA-256", "MGF1", MGF1ParameterSpec.SHA256, PSource.PSpecified.DEFAULT));
         String dec = new String(cipher.doFinal(Base64.getDecoder().decode(s)), "UTF-8");
  
         return dec;
