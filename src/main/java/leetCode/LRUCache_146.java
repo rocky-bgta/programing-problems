a786a7406a674aa6e52975279811dbf8eb1e2b02
Salahin <rocky.bgta@gmail.com>
2023-11-13 14:47:53 +0600

    complate long waiting LRU cash

--- src/main/java/leetCode/LRUCache_146.java
+++ src/main/java/leetCode/LRUCache_146.java
@@ -1,23 +1,22 @@
 package leetCode;
 
-import dsa.data_structures.linked_lists.doubly_linked_list_implementation.Node;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class LRUCache_146 {
 
 
-    class Node{
+    class Node {
         int key;
         int value;
         Node next;
         Node prev;
+
     }
 
-     Node head = new Node();
-     Node tail = new Node();
-     Map<Integer, Node> nodeMap;
+    Node head = new Node();
+    Node tail = new Node();
+    Map<Integer, Node> nodeMap;
     int cashCapacity;
 
     public LRUCache_146(int capacity) {
@@ -28,21 +27,46 @@ public class LRUCache_146 {
     }
 
     public int get(int key) {
-       return  0;
+        int result = -1;
+        Node node = nodeMap.get(key);
+        if (node != null) {
+            result = node.value;
+            removeNode(node);
+            addToFirst(node);
+        }
+        return result;
     }
 
     public void put(int key, int value) {
+        Node node = nodeMap.get(key);
+        if (node != null) {
+            removeNode(node);
+            node.value = value;
+            addToFirst(node);
+        } else {
+            if (nodeMap.size() == cashCapacity) {
+                nodeMap.remove(tail.prev.key);
+                removeNode(tail.prev);
+            }
 
+            Node newNode = new Node();
+            newNode.key = key;
+            newNode.value = value;
+            nodeMap.put(key, newNode);
+            addToFirst(newNode);
+        }
     }
 
     public void addToFirst(Node node) {
-        Node newNode = new Node();
-        head.prev = newNode;
-        newNode.next = head;
-        head = newNode;
+        Node headNext = head.next;
+        node.next = headNext;
+        headNext.prev = node;
+
+        head.next = node;
+        node.prev = head;
     }
 
-    public void removeNode(Node node){
+    public void removeNode(Node node) {
         Node nextNode = node.next;
         Node prevNode = node.prev;
 
