50233680c5e918fadb1d6dfd9865ebe5e06ad918
Salahin <rocky.bgta@gmail.com>
2023-11-21 10:24:51 +0600

    testcase working

--- src/main/java/leetCode/PermutationInString_567.java
+++ src/main/java/leetCode/PermutationInString_567.java
@@ -34,7 +34,12 @@ false
 public class PermutationInString_567 {
 
     public static void main(String[] args) {
-        String s1 = "ab", s2 = "eidbaooo";
+        String s1 = "ab", s2 = "eidboaoo";
+       // String s1 = "hello", s2="ooolleoooleh";
+
+//        Input: s1 = "ab", s2 = "eidbaooo"
+//        Output: true
+
         System.out.println(checkInclusion(s1, s2));
     }
 
@@ -42,18 +47,17 @@ public class PermutationInString_567 {
         Map<Character, Integer> charFrequency = new HashMap<>();
         Map<Character, Integer> windowFrequency = new HashMap<>();
         int windowLength = s1.length();
-        boolean result=true;
+        boolean result=false;
         for (int i = 0; i < windowLength; i++) {
             if (!charFrequency.containsKey(s1.charAt(i))) {
                 charFrequency.put(s1.charAt(i), 1);
             } else {
-                charFrequency.put(s1.charAt(i), charFrequency.get(i) + 1);
+                charFrequency.put(s1.charAt(i), charFrequency.getOrDefault(i,0) + 1);
             }
         }
 
         // sliding window with give length logic
         int left = 0;
-        int right = s1.length()-1;
         int count = 0;
         int windowSize = s1.length();
 
@@ -61,47 +65,43 @@ public class PermutationInString_567 {
         for(int i=0; i<s2.length(); i++){
             while(count<windowSize && left<s2.length()-1){
                 System.out.println(s2.charAt(left)+"-->");
+
+
+                if (charFrequency.containsKey(s2.charAt(left))) {
+
+                    if (!windowFrequency.containsKey(s2.charAt(left))) {
+                        windowFrequency.put(s2.charAt(left), 1);
+                    } else {
+                        windowFrequency.put(s2.charAt(left), windowFrequency.getOrDefault(s2.charAt(left),0) + 1);
+                    }
+
+                }
+
+                if (charFrequency.size() == windowFrequency.size()) {
+                    for (Map.Entry<Character, Integer> entry : charFrequency.entrySet()) {
+
+                        if (windowFrequency.get(entry.getKey()) != entry.getValue()) {
+                            result = false;
+                        }else {
+                            result = true;
+                        }
+                    }
+
+                }
+
+                if(result){
+                    return result;
+                }
+
                 left++;
                 count++;
             }
             left--;
             count=0;
-        }
-
-
-        int index = 0;
-        while (index < s2.length()) {
-            for (int j = 0; j < windowLength; j++) {
-                System.out.println(s2.charAt(index));
-
-//                if (charFrequency.containsKey(s2.charAt(index))) {
-//
-//                    if (!windowFrequency.containsKey(s2.charAt(index))) {
-//                        windowFrequency.put(s2.charAt(index), 1);
-//                    } else {
-//                        windowFrequency.put(s2.charAt(index), windowFrequency.get(s2.charAt(index)) + 1);
-//                    }
-//
-//                }
-                index++;
-            }
-
-
-
-            //if (charFrequency.size() == windowFrequency.size()) {
-//                for (Map.Entry<Character, Integer> entry : charFrequency.entrySet()) {
-//
-//                    if (windowFrequency.get(entry.getKey()) != entry.getValue()) {
-//                        result = false;
-//                    }
-//                }
-
-            //}
             windowFrequency.clear();
         }
 
         return result;
-
     }
 }
 
