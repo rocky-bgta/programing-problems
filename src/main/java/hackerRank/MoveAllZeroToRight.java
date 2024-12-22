534b03836690a82527d0a593c4e026f90d7e3eeb
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2024-12-22 09:05:56 +0600

    make some update move all zero to right

--- src/main/java/hackerRank/MoveAllZeroToRight.java
+++ src/main/java/hackerRank/MoveAllZeroToRight.java
@@ -6,25 +6,26 @@ package hackerRank;
 
 
 public class MoveAllZeroToRight {
-
     public static void main(String[] args) {
-
         int ar[] = {1, 0, 8, 9, 0, 7, 2, 0};
-        int j;
+        int pos = 0; // Points to the position where the next non-zero should go
+
+        // Move non-zero elements to the left
         for (int i = 0; i < ar.length; i++) {
-            if (ar[i] == 0) {
-                j = i;
-                while (j < ar.length-1) {
-                    ar[j] = ar[j + 1];
-                    ar[ar.length - 1] = 0;
-                    j++;
-                }
+            if (ar[i] != 0) {
+                ar[pos++] = ar[i];
             }
         }
 
+        // Fill the rest of the array with zeros
+        while (pos < ar.length) {
+            ar[pos++] = 0;
+        }
+
+        // Print the result
         for (int item : ar) {
             System.out.print(item + " ");
         }
-
     }
 }
+
